baae13c06e9ca7e530b8e00b911f79fc30ca3c8f
zoraaga1 <fati.sousse12@gmail.com>
2024-07-04 20:49:40 +0100

    task 0-1

---
+++ 0x00-personal_data/README.md
@@ -0,0 +1,3 @@
+# 0x00-personal_data PROJECT
+[]: # To the actual path of the file in the repo:
+[]: # # alx-backend-user-data/0x00-personal_data/README.md
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""filtered_logger module"""
+
+import re
+import logging
+
+def filter_datum(fields: str, redaction: str, message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    for field in fields.split(','):
+        message = re.sub(rf'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: str):
+        """initialize RedactingFormatter"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+        
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records using filter_datum"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
