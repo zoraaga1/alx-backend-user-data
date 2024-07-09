d1220cbe2608d62183c8c6669867d54d36bc2204
zoraaga1 <fati.sousse12@gmail.com>
2024-07-09 21:53:14 +0100

    task1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ Unauthorized handler
+    """
+    abort(401)
