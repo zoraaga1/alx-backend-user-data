24fe71ae95c6ad15f3830a2b06dc9667a31486df
zoraaga1 <fati.sousse12@gmail.com>
2024-07-10 00:31:25 +0100

    task5

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,7 +14,10 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth = os.getenv("AUTH_TYPE")
-if auth:
+if auth == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module of Basic Auth views"""
+
+
+class BasicAuth:
+    """ BasicAuth class
+    """
+    pass
