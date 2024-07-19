9e50a25fb303862e1409d0f008cadbb4822e8cd1
zoraaga1 <fati.sousse12@gmail.com>
2024-07-19 20:00:35 +0100

    task0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,22 +2,19 @@
 """user module"""
 
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
-from sqlalchemy.orm import sessionmaker, mapper
+from sqlalchemy.orm import mapper
 
-# Initialize the metadata
 metadata = MetaData()
 
-# Define the table
 users_table = Table(
     'users', metadata,
-    Column('id', Integer, primary_key=True, index=True),
-    Column('email', String, nullable=False),
-    Column('hashed_password', String, nullable=False),
-    Column('session_id', String, nullable=True),
-    Column('reset_token', String, nullable=True),
+    Column('id', Integer, primary_key=True),
+    Column('email', String(250), nullable=False),
+    Column('hashed_password', String(250), nullable=False),
+    Column('session_id', String(250), nullable=True),
+    Column('reset_token', String(250), nullable=True),
 )
 
-# Define the class
 class User:
     def __init__(self, email, hashed_password, session_id=None, reset_token=None):
         self.email = email
@@ -25,5 +22,4 @@ class User:
         self.session_id = session_id
         self.reset_token = reset_token
 
-# Map the class to the table
 mapper(User, users_table)
