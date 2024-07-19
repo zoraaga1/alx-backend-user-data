6776977704ec8742ca946aa3ed0bf8fe06887c88
zoraaga1 <fati.sousse12@gmail.com>
2024-07-19 21:17:14 +0100

    task1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,8 +31,7 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to the database
-        """
+        """Add a user to the database and return the User object."""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
